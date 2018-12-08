7a94c20b33820482f3abfd554c07a80dfef0f1da
Danijela Milanko <ddmilanko@gmail.com>
2018-12-08 15:40:58 +0100

    main layout

---
+++ src/containers/Chat/Chat.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class Chat extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>
+                    Chat
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Chat;
